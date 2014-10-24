3404e86e457cf76263627f04883d451cd07240ef
Alex Shapovalov <alex@yaplex.com>
2014-10-23 22:39:16 -0400

    nuget packager + first version

--- Yaplex.Azure.Storage.Nuget.Packager/Properties/AssemblyInfo.cs
+++ Yaplex.Azure.Storage.Nuget.Packager/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yaplex.Azure.Storage")]
-[assembly: AssemblyDescription("Simplified work with Azure tables.")]
+[assembly: AssemblyTitle("Yaplex.Azure.Storage.Nuget.Packager")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Yaplex Inc.")]
-[assembly: AssemblyProduct("Yaplex.Azure.Storage")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yaplex.Azure.Storage.Nuget.Packager")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8cfcc835-c49a-4a55-93a2-7f983f95c368")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
